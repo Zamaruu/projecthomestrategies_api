6affdfa45a6ed97bf61ca20704ec79cb0e550514
Maximilian Ditz <max.ditz@outlook.de>
2022-01-18 20:12:43 +0100

    signup response and conflict response chamged

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -2,14 +2,17 @@
 using HomeStrategiesApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using projecthomestrategies_api.Helper;
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace projecthomestrategies_api.Controllers
 {
@@ -38,9 +41,13 @@ namespace projecthomestrategies_api.Controllers
                 var user = GetUserFromBase64Credential(credentials);
 
                 if (user == null)
-                    return BadRequest("User not found");
+                    return Unauthorized("User not found");
 
-                return Ok(GenerateToken(user));
+                Dictionary<string, dynamic> signInResult = new Dictionary<string, dynamic>();
+                signInResult.Add("user", user);
+                signInResult.Add("token", GenerateToken(user));
+
+                return Ok(signInResult);
             }
         }
 
@@ -56,6 +63,11 @@ namespace projecthomestrategies_api.Controllers
                 Type = UserType.Basic,
             };
 
+            if (CheckIfUserExistsWithMail(newUser.Email))
+            {
+                return Conflict("Benutzer mit dieser Mail existiert bereits");
+            }
+
             _context.User.Add(newUser);
             _context.SaveChanges();
 
@@ -66,7 +78,7 @@ namespace projecthomestrategies_api.Controllers
             }
             else
             {
-                return Ok(newlyCreatedUser);
+                return Created("", newlyCreatedUser);
             }
         }
 
@@ -108,7 +120,18 @@ namespace projecthomestrategies_api.Controllers
                 var password = credentials[1];
 
                 var user = _context.User
+                                .Include(u => u.Household)
                                 .Where(u => u.Email.Equals(email) && u.Password.Equals(password))
+                                .Select(u => new User
+                                {
+                                    UserId = u.UserId,
+                                    Firstname = u.Firstname,
+                                    Surname = u.Surname,
+                                    Email = email,
+                                    UserColor = u.UserColor,
+                                    Household = u.Household,
+
+                                })
                                 .FirstOrDefault();
 
                 if (user == null)
@@ -144,6 +167,17 @@ namespace projecthomestrategies_api.Controllers
 
             var systemToken = tokenHandler.CreateToken(tokenDescriptor);
             return tokenHandler.WriteToken(systemToken);
+        }
+
+        private bool CheckIfUserExistsWithMail(string email)
+        {
+            var response = _context.User.Where(u => u.Email.Equals(email)).FirstOrDefault();
+
+            if(response != null)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -86,7 +86,7 @@ namespace projecthomestrategies_api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "projecthomestrategies_api v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
