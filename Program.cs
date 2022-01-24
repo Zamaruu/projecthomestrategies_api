a99f3b4f9fbdfb7b22e2e7d11da15bfb69663e0e
Maximilian Ditz <max.ditz@outlook.de>
2022-01-24 09:53:14 +0100

    make local api run open to network for testing

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ namespace projecthomestrategies_api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://0.0.0.0:5000;https://0.0.0.0:5001");
+                    webBuilder.UseKestrel();
                     webBuilder.UseStartup<Startup>();
                 });
     }
